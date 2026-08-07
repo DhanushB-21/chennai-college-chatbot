dc0450985c870ff5bf51634ea5b6d8c12b4121b4
DhanushB-21 <dhanushbaskarv18@gmail.com>
2026-08-07 15:49:56 +0530

    Update chatbotclg.py

--- chatbotclg.py
+++ chatbotclg.py
@@ -5,7 +5,7 @@ st.set_page_config(
     page_title="College Bot",
     page_icon="🎓",
     layout="wide",
-    initial_sidebar_stage="expanded"
+    initial_sidebar_state="expanded"
 )
 
 # ---------- Load knowledge base ----------
